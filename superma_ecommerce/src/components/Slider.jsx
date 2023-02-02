eeec4fa8a243dea358205c32f1bf639f7d9cabb5
Thisara Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2023-02-02 09:59:05 +0530

    day3 ui

--- superma_ecommerce/src/components/Slider.jsx
+++ superma_ecommerce/src/components/Slider.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -26,41 +27,91 @@ const Arrow = styled.div`
 `;
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transform: translateX(0vw);
+  //align-items: center;
 `;
 const Slide = styled.div`
   display: flex;
   align-items: center;
   width: 100vw;
   height: 100vh;
+  background-color: ${props=>props.bg};
 `;
 const ImgContainer = styled.div`
   flex: 1;
   height: 100%;
 `;
 const Image = styled.img`
-    height: 80%;
+  height: 80%;
 `;
 const InfoContainer = styled.div`
   flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `;
 
 const Slider = () => {
+  const handleClick = (direction)=>{}
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={()=>{handleClick("left")}}>
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper>
-        <Wrapper>
+        <Slide bg="white">
           <ImgContainer>
             <Image
               src={require("../assets/favpng_fashion-model-ready-to-wear-clothing-dress.png")}
             />
           </ImgContainer>
-          <InfoContainer></InfoContainer>
-        </Wrapper>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="red">
+          <ImgContainer>
+            <Image
+              src={require("../assets/favpng_fashion-model-ready-to-wear-clothing-dress.png")}
+            />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="yellow">
+          <ImgContainer>
+            <Image
+              src={require("../assets/favpng_fashion-model-ready-to-wear-clothing-dress.png")}
+            />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>POPULAR SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
       </Wrapper>
-      <Arrow direction="right">
+      dont eget comromise dnt get co
+      <Arrow direction="right" onClick={()=>{handleClick("right")}}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
